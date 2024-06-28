c06ff4b1b385e15e7f87dfd4980a1f3971970c9d
Aliyeff01 <arazpal@code.edu.az>
2024-06-28 04:39:16 +0400

    Add files via upload

---
+++ Task5/script.js
@@ -0,0 +1,13 @@
+let leg1 = 3
+let leg2 = 4
+let hypotenuse = 5
+
+let leg1Squared = leg1 * leg1
+let leg2Squared = leg2 * leg2
+let hypotenuseSquared = hypotenuse * hypotenuse
+
+if (leg1Squared + leg2Squared === hypotenuseSquared) {
+    console.log("Conclusion: it is a right triangle");
+} else {
+    console.log("Conclusion: it is not a right triangle");
+}
